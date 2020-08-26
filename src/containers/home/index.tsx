eb0de00607d0c8f600548cbb1b294923fc985b98
jean sidharta <jeansidharta@gmail.com>
2020-08-25 22:59:29 -0300

    Created JSON-LD for the home page

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -9,6 +9,7 @@ import HomePlans from '../../components/layout/home/plans';
 import HomeMonadaTeam from '../../components/layout/home/monada-team';
 import HomeContact from '../../components/layout/home/contact';
 import deployedUrl from '../../constants/deployed-url';
+import ImageURLS from '../../images';
 
 const Main = styled.div`
 	width: 100%;
@@ -17,6 +18,43 @@ const Main = styled.div`
 	overflow-y: auto;
 `;
 
+const JSONLD = `{
+	"@context": "http://schema.org/",
+	"@type": "Corporation",
+	"@id": "https://monada.tech/",
+	"url": "https://monada.tech/",
+	"name": "Mônada",
+	"image": "${ImageURLS.logo.main}",
+	"email": "contato@monada.tech",
+	"founder": [
+		{
+			"@type": "Person",
+			"name": "Reijane Salazar",
+			"gender": "Female",
+			"jobTitle": "Pesquisadora",
+			"image": "${ImageURLS.founders.reijane}",
+			"familyName": "Salazar"
+		},
+		{
+			"@type": "Person",
+			"name": "Maria Luiza Salvador",
+			"gender": "Female",
+			"jobTitle": "Designer",
+			"image": "${ImageURLS.founders.maria}",
+			"familyName": "Salvador"
+		},
+		{
+			"@type": "Person",
+			"name": "Adla Viana",
+			"gender": "Female",
+			"jobTitle": "Pesquisadora",
+			"image": "${ImageURLS.founders.adla}",
+			"familyName": "Viana"
+		}
+	],
+	"logo": "${ImageURLS.logo.main}"
+}`;
+
 export default function Home() {
 	const { closeHamburguer } = useHamburguer();
 
@@ -24,7 +62,8 @@ export default function Home() {
 		<>
 			<Head>
 				<title>Mônada</title>
-				<link rel="canonical" href={`${deployedUrl}/home`}/>
+				<link rel='canonical' href={`${deployedUrl}/home`}/>
+				<script type='application/json-ld'>{JSONLD}</script>
 			</Head>
 			<Navbar />
 			{/* Note: The ID is used by the navbar to scroll this element's contents into view. */}
