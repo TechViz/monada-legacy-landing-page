de21246f5fe5606c8f72f8664e7ba8ea4ee7fbdd
jean sidharta <jeansidharta@gmail.com>
2020-08-27 16:55:36 -0300

    json-ld fix

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -19,40 +19,45 @@ const Main = styled.div`
 `;
 
 const JSONLD = {
-	"@context": "http://schema.org/",
-	"@type": "Corporation",
-	"@id": "https://monada.tech/",
-	"url": "https://monada.tech/",
-	"name": "Mônada",
-	"image": `${deployedUrl}${ImageURLS.logo.png.main}`,
-	"email": "contato@monada.tech",
-	"founder": [
+	'@context': 'http://schema.org/',
+	'@type': 'Organization',
+	'@id': 'https://monada.tech/',
+	'url': 'https://monada.tech/',
+	'name': 'Mônada',
+	'image': `${deployedUrl}${ImageURLS.logo.png.main}`,
+	'email': 'contato@monada.tech',
+	'founder': [
 		{
-			"@type": "Person",
-			"name": "Reijane Salazar",
-			"gender": "Female",
-			"jobTitle": "Pesquisadora",
-			"image": `${deployedUrl}${ImageURLS.founders.reijane}`,
-			"familyName": "Salazar"
+			'@type': 'Person',
+			'name': 'Reijane Salazar',
+			'gender': 'Female',
+			'jobTitle': 'Pesquisadora',
+			'image': `${deployedUrl}${ImageURLS.founders.reijane}`,
+			'familyName': 'Salazar',
 		},
 		{
-			"@type": "Person",
-			"name": "Maria Luiza Salvador",
-			"gender": "Female",
-			"jobTitle": "Designer",
-			"image": `${deployedUrl}${ImageURLS.founders.maria}`,
-			"familyName": "Salvador"
+			'@type': 'Person',
+			'name': 'Maria Luiza Salvador',
+			'gender': 'Female',
+			'jobTitle': 'Designer',
+			'image': `${deployedUrl}${ImageURLS.founders.maria}`,
+			'familyName': 'Salvador',
 		},
 		{
-			"@type": "Person",
-			"name": "Adla Viana",
-			"gender": "Female",
-			"jobTitle": "Pesquisadora",
-			"image": `${deployedUrl}${ImageURLS.founders.adla}`,
-			"familyName": "Viana"
-		}
+			'@type': 'Person',
+			'name': 'Adla Viana',
+			'gender': 'Female',
+			'jobTitle': 'Pesquisadora',
+			'image': `${deployedUrl}${ImageURLS.founders.adla}`,
+			'familyName': 'Viana',
+		},
 	],
-	"logo": `${deployedUrl}${ImageURLS.logo.png.main}`
+	'logo': {
+		'@type': 'ImageObject',
+		'name': 'Mônada Logo',
+		'contentUrl': `${deployedUrl}${ImageURLS.logo.png.main}`,
+		'url': `${deployedUrl}${ImageURLS.logo.png.main}`,
+	},
 };
 
 export default function Home() {
@@ -63,7 +68,9 @@ export default function Home() {
 			<Head>
 				<title>Mônada</title>
 				<link rel='canonical' href={`${deployedUrl}/home`}/>
-				<script type='application/ld+json'>{JSON.stringify(JSONLD)}</script>
+
+				{/* dangerouslySetInnerHTML is used to prevent string escaping */}
+				<script type='application/ld+json' dangerouslySetInnerHTML={{ __html: JSON.stringify(JSONLD) }}></script>
 
 				{/* These meta tags are related to OpenGraph, which allows for your better webpage cards. */}
 				<meta property='og:url' content={deployedUrl + '/'} />
